1b88f1e3a564e0c13a596de75a315ada68e026d5
sasakazu <sasakura.company@gmail.com>
2018-06-12 20:24:38 +0900

    picker controller setting ...

---
+++ wankoAlbum/pets/petCamera.swift
@@ -0,0 +1,41 @@
+//
+//  petCamera.swift
+//  wankoAlbum
+//
+//  Created by 笹倉 一也 on 2018/06/12.
+//  Copyright © 2018年 笹倉 一也. All rights reserved.
+//
+
+
+import UIKit
+
+class petCamera: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+    
+    
+    @IBAction func petCameraTapped(_ sender: Any) {
+        
+   
+        
+    }
+    
+    
+    @IBAction func petAlbumTapped(_ sender: Any) {
+        
+
+        
+    }
+  
+
+
+
+
+}
---
+++ wankoAlbum/pets/petImage.swift
@@ -0,0 +1,42 @@
+//
+//  petImage.swift
+//  wankoAlbum
+//
+//  Created by 笹倉 一也 on 2018/06/12.
+//  Copyright © 2018年 笹倉 一也. All rights reserved.
+//
+
+import UIKit
+
+class petImage: UIViewController {
+    
+    var petPreviewImage: UIImage?
+    
+    @IBOutlet weak var petImageView: UIImageView!
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        
+        
+    }
+  
+    
+    
+    @IBAction func petSaved(_ sender: Any) {
+        
+    
+    }
+    
+
+    
+
+
+}
